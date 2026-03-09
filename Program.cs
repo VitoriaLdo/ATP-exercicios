39339afed688c42586f29d0d0644fc33a4fade58
Vitoria Oliveira <vitorialdo12@gmail.com>
2026-03-09 20:34:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿/*exercicio 1
+
+double n1, n2, media;
+
+Console.Write("Digite sua primeira nota:");
+n1= int.Parse(Console.ReadLine());
+Console.Write("Digite sua sgunda nota:");
+n2= int.Parse(Console.ReadLine());
+
+media= (n1*2 + n2*3)/(2+3);
+
+Console.WriteLine("A media ponderada de suas notas é:" + media); */
+
+/*exercico 2 
+
+int preco;
+double novoPreco;
+
+Console.Write("Digite o preço do produto:");
+preco= int.Parse(Console.ReadLine());
+novoPreco = preco * 0.9;
+
+Console.WriteLine("O novo preço é: {0}", novoPreco); */
+
+/*
+double salario, aumento, novoSalario;
+
+Console.Write("Digite o seu salario atual:");
+salario= double.Parse(Console.ReadLine());
+
+aumento= salario * 0.15;
+
+novoSalario= salario + aumento;
+
+Console.WriteLine("O novo salario é: {0}", novoSalario); */
+
+double km, litro, consumo;
+
+Console.Write("Digite a quantidade de km a ser percorrido: ");
+km= double.Parse(Console.ReadLine());
+Console.Write("Digite a quantidade de km a ser percorrido: ");
+litro= double.Parse(Console.ReadLine());
+
+consumo= km/litro;
+
+Console.WriteLine("O consumo medio em km do seu carro é de: {0}", consumo);
+
+
+
+
+
